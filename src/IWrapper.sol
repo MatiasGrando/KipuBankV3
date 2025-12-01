94aed2e59f7199c874f01d3396948402616fd7ea
Matias Grando <matias.grando@hotmail.com>
2025-12-01 03:50:25 -0300

    Update IWrapper.sol

--- src/IWrapper.sol
+++ src/IWrapper.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity > 0.8.28;
 
-/// @notice Minimal interface for the Wrapper contract used to swap tokens -> USDC.
+/// @title Minimal interface for a token-to-USDC Wrapper
+/// @notice Defines the core functionality for swapping any ERC20 token into USDC
+/// @dev Implemented by the Wrapper contract, typically interacting with a DEX router
+
 interface IWrapper {
+
+    /// @notice Swaps a specified ERC20 token into USDC
+    /// @param tokenIn The address of the ERC20 token to swap from
+    /// @param amountIn The amount of tokenIn to swap
+    /// @param amountOutMin The minimum acceptable amount of USDC to receive (slippage protection)
+    /// @param recipient The address that will receive the resulting USDC
+    /// @return amountOut The amount of USDC actually received by recipient
     function swapToUsdc(
         address tokenIn,
         uint256 amountIn,
@@ -10,5 +20,10 @@ interface IWrapper {
         address recipient
     ) external returns (uint256 amountOut);
 
+
+    /// @notice Estimates the amount of USDC that would be received for a given token input 
+    /// @param tokenIn The address of the ERC20 token to swap from 
+    /// @param amountIn The amount of `tokenIn` to swap 
+    /// @return amountOut The estimated amount of USDC that would be received
     function previewSwapToUsdc(address tokenIn, uint256 amountIn) external view returns (uint256 amountOut);
 }
